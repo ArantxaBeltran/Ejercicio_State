14924334bd7922fac4e2cfc05f46d0d14db4771e
ArantxaBeltran <a.beltrang@academiadeartesguerrero.com>
2021-04-23 21:52:50 -0500

    Initial commit

---
+++ Assets/Scenes/Enemigo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    public int poder;
+    public MovementPlayer MovementPlayer;
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Player" )
+        {
+            MovementPlayer.vida = -poder;
+            
+                        
+        }
+    }
+}
---
+++ Assets/Scenes/GameManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    GameState_ currentGameSate = GameState_.Menu;
+    static GameManager gm;
+    public Transform positionin;
+    public Transform cube;
+    public MovementPlayer MovementPlayer;
+
+
+    private void Awake()
+    {
+       
+        gm = this;
+    }
+    public static GameManager GetIns()
+    {
+        return gm;
+    }
+
+    public enum GameState_
+    {
+        Menu,
+        InGame,
+        GameOver,
+    }
+    void ChangeGameState(GameState_ newGameSate_)
+    {
+        switch (newGameSate_)
+        {
+            case GameState_.Menu:
+
+                break;
+
+
+
+            case GameState_.InGame:
+
+                
+
+                break;
+
+
+            case GameState_.GameOver:
+
+               
+                
+                break;
+
+            default:
+
+                currentGameSate = GameState_.Menu;
+                break;
+
+        }
+
+        
+    }
+    public void StartGame_()
+    {
+        
+        ChangeGameState(GameState_.InGame);
+        MovementPlayer.Movimiento();
+        
+
+
+    }
+    public void GameOver()
+    {
+        ChangeGameState(GameState_.GameOver);
+
+        cube.position = positionin.position;
+        
+    }
+
+
+    public void Backintomenu()
+    {
+        ChangeGameState(GameState_.Menu);
+    }
+
+
+    private void Update()
+    {
+       if(currentGameSate != GameState_.InGame && Input.GetKey(KeyCode.B))
+        {
+            ChangeGameState(GameState_.InGame);
+            StartGame_();
+            
+        }
+        if (currentGameSate != GameState_.GameOver && MovementPlayer.vida <= 0)
+        {
+            ChangeGameState(GameState_.GameOver);
+            GameOver();
+        }
+
+
+    }
+    
+
+
+}
